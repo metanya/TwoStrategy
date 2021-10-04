c317da0c7eddf5c3eaac8fbc16dd7393fed10add
NaomiCreate <naomiresnik@gmail.com>
2021-10-04 17:53:51 +0300

    adding functions to parse file

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from record.record import Record
 from parsers.parser import Parser
 
 if __name__ == '__main__':
-    record = Record("AP006627").get_genbank_record()
+    record = Record("AP006627")
+    record.create_genbank_file()
+    AP006627_record_content = record.get_record_content()
+
     parser = Parser()
-    parser.get_data_frame("test.csv", record)
+    parser.get_data_frame("test.csv", AP006627_record_content)
--- record/record.py
+++ record/record.py
@@ -1,17 +1,38 @@
 from Bio import Entrez, SeqIO
-from constants import ENTREZ_EMAIL
+# from constants import ENTREZ_EMAIL
+from .constants import ENTREZ_EMAIL
+import os
 
 
 class Record:
     def __init__(self, record_id):
         self.record_id = record_id
 
-    Entrez.email = ENTREZ_EMAIL
-
     def get_genbank_record(self):
         with Entrez.efetch(db="nucleotide", id=self.record_id, rettype="gb", retmode="text") as handle:
+            # results = handle.read()
+            # print(type(results))
+            # return results
             list_of_records = []
             for record in SeqIO.parse(handle, "genbank"):
                 list_of_records.append(record)
-            print(type(list_of_records[0]))
+            # print(type(list_of_records[0]))
             return list_of_records[0]
+
+    def get_record_content(self):
+        file_name = '{}.gb'.format(self.record_id)
+        with open(file_name, "r") as handle:
+            for i, record_gb in enumerate(SeqIO.parse(handle, "genbank")):
+                print('Record number: {}\n============='.format(i))
+                print(record_gb)
+            return record_gb  # next(record_gb) # the last record
+
+    def create_genbank_file(self):
+        if not os.path.exists('{}.gb'.format(self.record_id)):  # if the file not exists
+
+            Entrez.email = ENTREZ_EMAIL
+
+            with Entrez.efetch(db="nucleotide", id=self.record_id, rettype="gb", retmode="text") as handle:
+                with open('{}.gb'.format(self.record_id), "w") as out_handle:
+                    out_handle.write(handle.read())
+                print("The file: {}.gb created".format(self.record_id))
